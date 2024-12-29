7aaaeffd59671a4503c6bb41fc8db7dc0ca875e5
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-12-29 17:28:41 +0530

    Update Component to handle events and manage form state

--- src/starter/04-events/index.tsx
+++ src/starter/04-events/index.tsx
@@ -1,9 +1,39 @@
+import { useState } from 'react';
+
 function Component() {
-  return (
-    <div>
-      <h2>React & Typescript</h2>
-      <h2>Events</h2>
-    </div>
-  );
+	const [text, setText] = useState('');
+	const [email, setEmail] = useState('');
+
+	const handleChange = (
+		e: React.ChangeEvent<HTMLInputElement>
+	) => {
+		setEmail(e.target.value);
+	};
+	return (
+		<div>
+			<h2>Events</h2>
+			<form className="form">
+				<input
+					type="text"
+					value={text}
+					className="form-input mb-4"
+					onChange={(e) => setText(e.target.value)}
+				/>{' '}
+				<input
+					type="email"
+					value={email}
+					className="form-input mb-4"
+					onChange={handleChange}
+				/>
+				<button
+					type="submit"
+					className="btn btn-block btn-secondary"
+				>
+					{' '}
+					Submit
+				</button>
+			</form>
+		</div>
+	);
 }
 export default Component;
